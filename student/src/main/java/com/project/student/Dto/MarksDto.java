90586cf5fb9b9b0115c9b95eaf2ececd43db6b75
Sri4457 <srihithamoturi2000@gmail.com>
2024-01-09 21:20:40 -0600

    changed lambook.

--- student/src/main/java/com/project/student/Dto/MarksDto.java
+++ student/src/main/java/com/project/student/Dto/MarksDto.java
@@ -1,31 +1,17 @@
 package com.project.student.Dto;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class MarksDto {
 
 	private String stdid;
 	private List<Double> marks;
 	private long id;
 	
-	public String getStdid() {
-		return stdid;
-	}
-	public void setStdid(String stdid) {
-		this.stdid = stdid;
-	}
-	public List<Double> getMarks() {
-		return marks;
-	}
-	public void setMarks(List<Double> marks) {
-		this.marks = marks;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
+
 	public MarksDto(String stdid, List<Double> marks) {
 		super();
 		this.stdid = stdid;
